676e9c2160c8659ca1ac6ca7587e6f3caeecc371
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2021-10-05 10:31:09 +0900

    change p tag

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
           <input />
           <button>button</button>
         </span>
-        <p></p>
+        <p style={{ border: "solid 1px", height: "20px", width: "40%" }}></p>
       </body>
     </div>
   );
